6c11dd4f719912cdb4607bd744d7b06b5ed8ec03
MarcosSchlu <mschluselblum@gmail.com>
2022-03-26 12:47:03 -0300

    getInfo fix

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const fetch = require('node-fetch');
 
 // MARCOS
 // IMPORTO DESDE BASE DE DATOS
@@ -10,29 +11,29 @@ const router = Router();
 // MARCOS
 // FUNCION ASYNCONA QUE TRAE TODA LA INFORMACION DE LA API y DE LA BASE DE DATOS (ICLUYENDO LOS TIPOS POR EL ATRIBUTO NOMBRE)
 const getInfo = async () => {
-  const api = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+  const api = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2')
   const infoApi = await api.json()
   const infoDB = await Pokemon.findAll({ include: Tipo })
 
   // CONCATENO LA INFO DE LA API CON LA INFO DE LA BASE DE DATOS
-  const infoTotal = infoApi.concat(infoDB)
+  let infoTotal = [ ...infoDB, ...infoApi.results]
 
-  // MAPEO TODA LA INFO Y LA PUSHEO CON LA INFO QUE NECESITO
-  const pokemonInfo = []
-  for( i = 0 ; i < infoTotal.lenght ; i++ ){
-    if (!infoTotal[i]) return pokemonInfo
-    if(infoTotal[i].url) {
-      const pokemon = await fetch(infoTotal[i].url)
-      const infoPokemon = await pokemon.json()
+  // OBTENGO TODA LA INFO Y LA PUSHEO CON LA INFO QUE NECESITO
+
+  let pokemonInfo = [];
+  for (i = 0; i < infoTotal.length; i++) {
+    if (!infoTotal[i]) return pokemonInfo;
+    if (infoTotal[i].url) {
+      const pokemon = await fetch(infoTotal[i].url);
+      const infoPokemon = await pokemon.json();
 
       pokemonInfo.push({
         id: infoPokemon.id,
         name: infoPokemon.name,
         tipo: infoPokemon.types.map((tipo) => tipo.type.name),
         img: infoPokemon.sprites.other.dream_world.front_default,
-        // VER SI ESTO ES FUERZA???
         fuerza: infoPokemon.stats[1].base_stat,
-      })
+      });
     } else {
       pokemonInfo.push({
         id: infoTotal[i].id,
@@ -49,6 +50,15 @@ const getInfo = async () => {
 }
 
 
-
+router.get('/pokemons', async ( req, res ) => {
+  const name = req.query.name
+  let pokemosTotales = await getInfo ();
+  if (name) {
+    let pokemonName = await pokemosTotales.includes( pokemons => pokemons.name.toLowerCase().includes(name))
+    pokemonName.lenght ? res.status(200).send(pokemonName) : res.status(400).send('No existe el Pokemon')
+  } else {
+    res.status(200).send(pokemosTotales)
+  }
+})
 
 module.exports = router;
